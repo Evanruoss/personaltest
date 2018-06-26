fed7c75c935288a56de5667958e6eb83e7765972
Evan <evan.ruoss@gmail.com>
2018-06-26 13:49:12 -0500

    commit two

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+func main() {
+
+	fmt.Printf("hello %s world %s %d \n", "Evan", "Ashton", 3)
+	spew.Dump("some shit")
+}
