78a4a422590adad6b3257f0b47ad71045e8ba830
Evan <evan.ruoss@gmail.com>
2018-07-01 17:52:27 -0500

    Playing with Spacetest

--- spacetest/main.go
+++ spacetest/main.go
@@ -14,7 +14,7 @@ func main() {
 	fmt.Printf("Derp derp derp derp line 4 \n \n")
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Launch Missile? y/n:")
+	fmt.Print("Launch Missile? [y/n]:")
 	_, _ = reader.ReadString('\n')
 	fmt.Println("FUCK YOU")
 }
