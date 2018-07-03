4341e82225ebe5677b9bda7a6b3a7749ed9dbd13
Evan <evan.ruoss@gmail.com>
2018-07-03 15:46:50 -0500

    playing with packages

---
+++ pkgtest/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+)
+
+func main() {
+  fmt.Println("Hellooo!")
+  fmt.Println(stringutil.Reverse("!oooolleH"))
+}
